0754d2a33a214352044b3446b86a3212cd99ef5e
Joe Nyland <joenyland@me.com>
2014-08-07 12:15:45 +0100

    Tidy up JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@
 
 $(document).ready(function () {
 
+    /* Add the current year to the footer */
     function getYear() {
         var d = new Date();
         var n = d.getFullYear();
         return n.toString();
     }
-
     $('#year').html(getYear());
 
     /* Enable Bootstrap tooltips */
