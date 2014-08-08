7d0c0d544f0f6729f45ead063d2f086e4857a0be
Joe Nyland <joenyland@me.com>
2014-08-08 16:30:45 +0100

    Revert "Random background image"
    This reverts commit eaa7832ade19cc000a502360dee004cf64e34e14.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,4 @@ $(document).ready(function () {
         window.history.back()
     }
 
-    /* Random Background Image */
-    var imagesArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
-    var num = Math.floor(Math.random() * imagesArray.length)
-    var url = 'url(assets/img/' + imagesArray[num] + ')';
-    $('.intro').css('background-image', url, 'no-repeat bottom center scroll')
-
 });
