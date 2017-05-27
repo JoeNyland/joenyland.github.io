86cdb465feb25574f0b703a55321770d9dda9342
Joe Nyland <joenyland@me.com>
2017-05-27 09:56:50 +0100

    Reformat JS

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,4 +1,4 @@
 // Random Gif on error page
-$(document).ready(function(){
-  $(".error-not-found").sort(function(){ return 0.5 - Math.random()}).first().show();
+$(document).ready(function() {
+  $(".error-not-found").sort(function() { return 0.5 - Math.random();}).first().show();
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-    // Scroll to top button
-    // Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('.btn#back-to-top').fadeIn();
-        } else {
-            $('.btn#back-to-top').fadeOut();
-        }
-    });
+$(document).ready(function() {
+  // Scroll to top button
+  // Check to see if the window is top if not then display button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $(".btn#back-to-top").fadeIn();
+    } else {
+      $(".btn#back-to-top").fadeOut();
+    }
+  });
 
-    // Click event to scroll to top
-    $('.btn#back-to-top').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
-    });
+  // Click event to scroll to top
+  $(".btn#back-to-top").click(function() {
+    $("html, body").animate({scrollTop: 0}, 800);
+    return false;
+  });
 });
