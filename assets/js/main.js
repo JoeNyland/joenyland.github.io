ca22f8828c9a18a8806a41417324bda2327cb3a0
Joe Nyland <joenyland@me.com>
2017-11-04 19:48:57 +0000

    Change ID of "back to the top" button as ABP was blocking it

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
   // Check to see if the window is top if not then display button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $(".btn#back-to-top").fadeIn();
+      $(".btn#scroll-to-the-top").fadeIn();
     } else {
-      $(".btn#back-to-top").fadeOut();
+      $(".btn#scroll-to-the-top").fadeOut();
     }
   });
 
   // Click event to scroll to top
-  $(".btn#back-to-top").click(function() {
+  $(".btn#scroll-to-the-top").click(function() {
     $("html, body").animate({scrollTop: 0}, 800);
     return false;
   });
